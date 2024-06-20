4544cc45e5da6016ea46d202e4a931c701652205
bardia <ali9090@gmail.com>
2024-06-20 16:56:40 +0330

    using useRef and useState with map for showing users

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import Navbar from './components/Navbar'
 
 function App() {
 
-  const [weather, setWeather] = useState();
+const[user , setUser] = useState([]);
 
-  const inputTag = useRef();
-  
-  function whatIsWeather() {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?appid=582560638c1b004bd0c8a5cd5e2f7efa&q=${inputTag.current.value}`)
-      .then(res => res.json())
-      .then(json=>setWeather(json))
-  }
+const users = useRef()
+
+function handleUser() {
+  fetch(`https://jsonplaceholder.typicode.com/users`)
+    .then(res => res.json())
+    .then(json => setUser(json))
+}
 
 
   return (
@@ -21,16 +21,17 @@ function App() {
         <Navbar />
         <div className='search-container'>
           <div className="search-inner">
-            <input type="search" ref={inputTag}  id='search-bar' placeholder='Enter Your Region' />
-            <button onClick={whatIsWeather}> Search </button>
+            <input type="search" id='search-bar' placeholder='Enter Your Region' />
+            <button onClick={handleUser}> Search </button>
           </div>
         </div>
-        <div className="api-info">
-          <h1>{weather && weather.name}</h1>
-          <h3>{weather && weather.sys.country}</h3>
-          <h2>{weather && weather.main.temp}</h2>
-          
-        </div>
+        {user.map((userInfo)=>(
+          <div className="api-info">
+            <h1>{userInfo && userInfo.name}</h1>
+            <h3><ins>{userInfo && userInfo.username}</ins></h3>
+            <h2>{userInfo && userInfo.email}</h2>
+          </div>
+        ))}
       </div>
     </div>
   )
